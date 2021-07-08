48604a991b146f054e62f2236f2240fe43003cae
Timur <tima@MacBook-Air-Timur.local>
2021-07-08 15:52:01 +0600

    Create json project

---
+++ reddit.py
@@ -0,0 +1,44 @@
+import requests
+import json
+
+
+
+def format_date(timestamp_object):
+    from datetime import datetime
+    datetime_object = datetime.fromtimestamp(timestamp_object)
+    
+    return str(datetime_object)
+
+
+def get_data(url):
+    response = requests.get(url,headers={'User-Agent':'your bot 0.1'})
+    
+    python_object = json.loads(response.text)
+    news = python_object['data']['children']
+    filtered_data = []
+    number = 1
+
+    for new in news:
+        news_data = {
+            f'News number: {number}' :
+            {
+                'title': new['data']['title'],
+                'author': new['data']['author'],
+                'created': format_date(new['data']['created'])
+            }
+        }
+        filtered_data.append(news_data)
+        number +=1
+
+    return filtered_data
+
+def write_to_json(data):
+    with open('RedditNews.json', 'w') as json_file:
+        json.dump(data,json_file, indent=4)
+
+
+def main(url):
+    data = get_data(url)
+    write_to_json(data)
+
+main('https://www.reddit.com/r/entertainment/.json')
